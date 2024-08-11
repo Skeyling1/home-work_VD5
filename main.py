a972e924e52381e55845e7a70b894d5d5c61b2a7
Skeyling1 <skeyling1@gmail.com>
2024-08-11 11:56:08 +0300

    final

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # context = {
-    #     "title" : "Йога-центр \"Гармония\""
-    # }
-    return render_template("home.html") #**context)
+    context = {
+        "link" : "Записаться"
+    }
+    return render_template("home.html", **context)
 
 @app.route("/about/")
 def about():
-    return render_template("about.html")
+    context = {
+        "link": "Оставить отзыв"
+    }
+    return render_template("about.html", **context)
 
 if __name__ == "__main__":
     app.run()
